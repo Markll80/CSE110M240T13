55353cf44262f3cd925fc183a3e0b6307d89082c
ericluo05 <eric_luo@Ymail.com>
2016-01-29 16:53:12 -0800

    activity setup

--- TritonProfessorRater/app/src/main/java/radarx/tritonprofessorrater/ForgotPassword.java
+++ TritonProfessorRater/app/src/main/java/radarx/tritonprofessorrater/ForgotPassword.java
@@ -6,32 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import com.parse.*;
-import com.parse.ParseObject;
 
-public class myActivity extends AppCompatActivity {
+public class ForgotPassword extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        // [Optional] Power your app with Local Datastore. For more info, go to
-// https://parse.com/docs/android/guide#local-datastore
-        Parse.enableLocalDatastore(this);
-        Parse.initialize(this);
-
-        //------------NOT WORKING
-        ParseUser.enableAutomaticUser();
-        ParseACL defaultACL = new ParseACL();
-// Optionally enable public read access.
-// defaultACL.setPublicReadAccess﴾true﴿;
-        ParseACL.setDefaultACL(defaultACL, true);
-
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_forgot_password);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
