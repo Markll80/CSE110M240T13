098fff110ae8292ebfa0439755ff266e94361860
Neil <neiluser@yahoo.com>
2016-01-30 13:14:04 -0800

    UI + functionality
    Added some UI, register button now should redirect to the register page

--- TritonProfessorRater/app/src/main/java/radarx/tritonprofessorrater/LogIn.java
+++ TritonProfessorRater/app/src/main/java/radarx/tritonprofessorrater/LogIn.java
@@ -1,29 +1,42 @@
 package radarx.tritonprofessorrater;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
-public class LogIn extends AppCompatActivity {
+public class LogIn extends AppCompatActivity implements View.OnClickListener{
+
+    Button bLogin;
+    Button bRegister;
+    EditText etEmail;
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        bLogin = (Button) findViewById(R.id.bLogin);
+        bRegister = (Button) findViewById(R.id.bRegister);
+        etEmail = (EditText) findViewById(etEmail);
+        etPassword = (EditText) findViewById(etPassword);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.bRegister:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
+
     }
 
 }
