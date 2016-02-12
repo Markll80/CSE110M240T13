ab3226cc7ad171ae0706a1b8c3e962fa611b05ec
Cherish MuMu <cherish.cui.122@gmail.com>
2016-02-11 23:55:33 -0800

    ProfPage with Layout Inflation
    Include ProfPage inflation on prof_page.

--- TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/ProfPage.java
+++ TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/ProfPage.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class ProfPage extends AppCompatActivity {
 
+    string[] comData=("Comment#1", "Comment#2", "Comment#3");
+    ListView l;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prof_page);
+        l=(ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, R.layout.single_comment, R.id.textView, comData);
+        l.setAdapter(adapter);
+
     }
 }
