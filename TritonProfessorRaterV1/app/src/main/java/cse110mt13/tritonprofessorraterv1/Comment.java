ddc0ff878d4a30994a7975bedec1860fa3f06d72
kathy31629 <kathy31629@gmail.com>
2016-02-12 16:09:22 -0800

    comment

--- TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/Comment.java
+++ TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/Comment.java
@@ -8,7 +8,38 @@ import com.parse.ParseObject;
 public class Comment extends ParseObject{
 
 
+    public void setup(String comment){
+        put("comment", comment);
+        put("numLikes", 0);
+    }
 
+    public String getComment() {
+        return getString("Comment");
+    }
 
+    public int getNumLikes() {
+        return getInt("numLikes");
+    }
 
+    public void putComment(String comment){
+        put("comment",comment);
+    }
+
+    public void addNumLikes(){
+        put("numLikes",getNumLikes()+1);
+    }
+
+    public void setComment(String comment, int numLikes){
+        put("comment", comment);
+        put("numLikes", numLikes);
+    }
+
+    public void addComment(String comment){
+        add("comments",comment);
+    }
+
+    @Override
+    public String toString(){
+        return getComment() + "\nLikes: "+getNumLikes();
+    }
 }
