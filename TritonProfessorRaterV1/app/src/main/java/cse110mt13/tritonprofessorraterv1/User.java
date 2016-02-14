c4ad2ae9d475a16346da9462dbf2ce476a474795
Cherish MuMu <cherish.cui.122@gmail.com>
2016-02-14 15:10:14 -0800

    The User Page Leave Blank for now
    Wait for adding ParseUser Info in the future.

--- TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/User.java
+++ TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/User.java
@@ -1,7 +1,13 @@
 package cse110mt13.tritonprofessorraterv1;
 
-/**
- * Created by Cherish on 16/2/14.
- */
-public class User {
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import java.text.ParseException;
+
+@ParseClassName("User")
+public class User extends ParseUser {
+
 }
