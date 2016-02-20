5f8700dfe9edec4df35e89983f8d8ba0ac826945
kathy31629 <kathy31629@gmail.com>
2016-02-19 21:12:07 -0800

    AddProf.java+AddComment.java

--- TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/AddComment.java
+++ TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/AddComment.java
@@ -8,9 +8,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RatingBar;
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class AddComment extends AppCompatActivity {
+
+    EditText acCourseName, acComment;
+    RatingBar ac_RatingC, ac_RatingE, ac_RatingH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,13 @@ public class AddComment extends AppCompatActivity {
         setContentView(R.layout.activity_add_comment);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        acCourseName = (EditText) findViewById(R.id.ac_AddCourse_ET);
+        acComment = (EditText) findViewById(R.id.ac_Comment_ET);
+
+        ac_RatingC = (RatingBar) findViewById(R.id.ac_RatingC);
+        ac_RatingE = (RatingBar) findViewById(R.id.ac_RatingE);
+        ac_RatingH = (RatingBar) findViewById(R.id.ac_RatingH);
 
         findViewById(R.id.ac_Submit_B).setOnClickListener(onclickListener);
         findViewById(R.id.ac_Cancel_B).setOnClickListener(onclickListener);
@@ -32,6 +47,17 @@ public class AddComment extends AppCompatActivity {
                      /*  TODO: write backend function to determine whether clicking on search
                    should jump to none found page, or search-found page
                  */
+
+                    String ac_Course, ac_Comment;
+                    int ac_C, ac_E, ac_H;
+
+                    ac_Course = acCourseName.getText().toString();
+                    ac_Comment = acComment.getText().toString();
+
+                    ac_C = (int) ac_RatingC.getRating();
+                    ac_E = (int) ac_RatingE.getRating();
+                    ac_H = (int) ac_RatingH.getRating();
+                    
                     finish(); //end current activity
                     startActivity(new Intent(AddComment.this, ProfPage.class)); //create a new activity
                     break;
--- TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/AddProf.java
+++ TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/AddProf.java
@@ -7,8 +7,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class AddProf extends AppCompatActivity {
+
+    EditText ProfName, apCourseName, apComment;
+    RatingBar ap_RatingC, ap_RatingE, ap_RatingH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +28,17 @@ public class AddProf extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        findViewById(R.id.ap_sumbit_B).setOnClickListener(onclickListener);;
-        findViewById(R.id.ap_cancel_B).setOnClickListener(onclickListener);;
-    }
+        ProfName = (EditText) findViewById(R.id.ap_name_ET);
+        apCourseName = (EditText) findViewById(R.id.ap_course_ET);
+        apComment = (EditText) findViewById(R.id.ap_comment_ET);
 
+        ap_RatingC = (RatingBar) findViewById(R.id.ap_RatingC);
+        ap_RatingE = (RatingBar) findViewById(R.id.ap_RatingE);
+        ap_RatingH = (RatingBar) findViewById(R.id.ap_RatingH);
+
+        findViewById(R.id.ap_sumbit_B).setOnClickListener(onclickListener);
+        findViewById(R.id.ap_cancel_B).setOnClickListener(onclickListener);
+    }
 
     private View.OnClickListener onclickListener = new View.OnClickListener() {
         @Override
@@ -30,6 +48,23 @@ public class AddProf extends AppCompatActivity {
                      /*  TODO: write backend function to determine whether clicking on search
                    should jump to none found page, or search-found page
                  */
+
+                    String ap_Prof, ap_Course, ap_Comment;
+                    int ap_C, ap_E, ap_H;
+
+                    ap_Prof = ProfName.getText().toString();
+                    ap_Course = apCourseName.getText().toString();
+                    ap_Comment = apComment.getText().toString();
+                    ap_C = (int) ap_RatingC.getRating();
+                    ap_E = (int) ap_RatingE.getRating();
+                    ap_H = (int) ap_RatingH.getRating();
+
+                    // test rating num
+                    /*
+                    String text = "" + ap_C;
+                    Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+                    */
+
                     finish(); //end current activity
                     startActivity(new Intent(AddProf.this, ProfPage.class)); //create a new activity
                     break;
@@ -40,4 +75,12 @@ public class AddProf extends AppCompatActivity {
             }
         }
     };
+
+    private void createProf(String apName, int apRatingC, int apRatingE, int apRatingH){
+        Professor newProf = new Professor();
+        newProf.setup(apName);
+        newProf.addRating(apRatingC,apRatingE,apRatingH);
+
+        //TODO:add comment
+    }
 }
