6ac11025f9593f35a3bc24e691b8bba5e4544faa
rdeng <rdeng@ucsd.edu>
2016-02-20 16:17:58 -0800

    update in loginTest

--- TritonProfessorRaterV1/app/src/androidTest/java/cse110mt13/tritonprofessorraterv1/espressoTest/loginTest.java
+++ TritonProfessorRaterV1/app/src/androidTest/java/cse110mt13/tritonprofessorraterv1/espressoTest/loginTest.java
@@ -1,14 +1,18 @@
 package cse110mt13.tritonprofessorraterv1.espressoTest;
 
 import android.app.Application;
+import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+import android.test.ViewAsserts;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -31,5 +35,8 @@ public class loginTest{
         String username = "admin";
         String password = "123456";
         onView(withId(R.id.input_email)).perform(typeText(username));
+        onView(withId(R.id.input_password)).perform(typeText(password));
+        onView(withId(R.id.btn_login)).perform(click());
+        onView(withId(R.layout.activity_main)).check(ViewAssertions.matches(isDisplayed()));
     }
 }
