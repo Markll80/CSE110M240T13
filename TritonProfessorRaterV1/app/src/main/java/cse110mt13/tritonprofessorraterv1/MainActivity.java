52bf04effc1cd272472c1255b95237de20fd606c
Cherish MuMu <cherish.cui.122@gmail.com>
2016-02-21 06:28:54 -0800

    Modified Main for Action Bar

--- TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/MainActivity.java
+++ TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/MainActivity.java
@@ -1,13 +1,23 @@
 package cse110mt13.tritonprofessorraterv1;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Handler;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -57,6 +67,13 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment main = new MainFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, main);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.addToBackStack(null);
+        ft.commit();
 
         setupSearchButton();
 
@@ -109,6 +126,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     class MyAdapter extends ArrayAdapter<String>
@@ -163,7 +181,51 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(searchIntent); //create a new activity*/
             }
         });
-        ;
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_call:
+                Intent dialer= new Intent(Intent.ACTION_DIAL);
+                startActivity(dialer);
+                return true;
+            case R.id.action_speech:
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                startActivityForResult(intent, 1234);
+
+                return true;
+            case R.id.action_contacts:
+                Toast.makeText(getApplicationContext(),"Contacts Clicked",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(),"Settings Clicked",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_status:
+                Toast.makeText(getApplicationContext(),"Status Clicked",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1234 && resultCode == RESULT_OK) {
+            String voice_text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0);
+            Toast.makeText(getApplicationContext(),voice_text,Toast.LENGTH_LONG).show();
+
+        }
+    }
 }
