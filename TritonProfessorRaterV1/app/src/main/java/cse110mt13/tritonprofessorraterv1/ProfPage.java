3622d5ffffc0c1a52c96432637abada891eef621
onlybaby <t8gu@ucsd.edu>
2016-02-24 22:41:40 -0800

    ProfPage update

--- TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/ProfPage.java
+++ TritonProfessorRaterV1/app/src/main/java/cse110mt13/tritonprofessorraterv1/ProfPage.java
@@ -86,47 +86,48 @@ public class ProfPage extends AppCompatActivity {
             courseName[i] = coList.get(i).getCourseName();
         }
         list= (ListView) findViewById(R.id.list_view_prof_comment);
-        list.setOnTouchListener(new View.OnTouchListener() {
-                                    // Setting on Touch Listener for handling the touch inside ScrollView
-                                    @Override
-                                    public boolean onTouch(View v, MotionEvent event) {
-                                        // Disallow the touch request for parent scroll on touch of child view
-                                        v.getParent().requestDisallowInterceptTouchEvent(true);
-                                        return false;
-                                    }
-                                });
-        setListViewHeightBasedOnChildren(list);
+
+
+
 
             MyAdapter adapter = new MyAdapter(this, courseName, num, comData);
             list.setAdapter(adapter);
+        list.setOnTouchListener(new View.OnTouchListener() {
 
+            // Setting on Touch Listener for handling the touch inside ScrollView
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("Test1", "Here!");
+                // Disallow the touch request for parent scroll on touch of child view
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+        ProfPage.setListViewHeightBasedOnChildren(list);
 
             //   getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         }
 
-    /**** Method for Setting the Height of the ListView dynamically.
-     **** Hack to fix the issue of not showing all the items of the ListView
-     **** when placed inside a ScrollView  ****/
-    public static void setListViewHeightBasedOnChildren(ListView listView) {
+    private static void setListViewHeightBasedOnChildren(ListView listView) {
+        Log.d("Test2", "Welcome!");
+
         ListAdapter listAdapter = listView.getAdapter();
-        if (listAdapter == null)
+        if (listAdapter == null) {
             return;
-
-        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
-        int totalHeight = 0;
-        View view = null;
-        for (int i = 0; i < listAdapter.getCount(); i++) {
-            view = listAdapter.getView(i, view, listView);
-            if (i == 0)
-                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, AbsListView.LayoutParams.WRAP_CONTENT));
-
-            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
-            totalHeight += view.getMeasuredHeight();
         }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
         ViewGroup.LayoutParams params = listView.getLayoutParams();
-        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        params.height = totalHeight
+                + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
         listView.setLayoutParams(params);
     }
         class MyAdapter extends ArrayAdapter<String>
@@ -148,11 +149,10 @@ public class ProfPage extends AppCompatActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-            View row = inflater.inflate(R.layout.prof_layout, parent, false);
+            View row = inflater.inflate(R.layout.linear_prof_layout, parent, false);
             TextView myNum= (TextView)row.findViewById(R.id.numOfLikes);
             TextView myCourseName = (TextView) row.findViewById(R.id.className);
             TextView myComment = (TextView) row.findViewById(R.id.commentText);
-          //  myNum.setText(numArray[position]);             //TODO : CAUSING CRASH
             myCourseName.setText(courseArray[position]);
             myComment.setText(comArray[position]);
             myNum.setText(""+num[position]);
